879fba2fec831d24b87c011cc8a1af3e692d6a09
Brandon McCoy <brandon.mccoy@slu.edu>
2023-11-18 22:45:48 -0600

    Adding first draft Reference APIs to main

--- Backend/backendAcademic/index.js
+++ Backend/backendAcademic/index.js
@@ -235,3 +235,91 @@ Function: Delete a task based on a given taskId
         }
     });
 });
+
+
+//
+// References call functions
+//
+/* 
+Function: Create a project based on a provided name, and return the id.
+Return: Reference's MondoDB _id
+*/
+app.post('/api/user/createReference', multer().none(), (request, response) => {
+    database.collection("References").countDocuments({}, (error, numofDocs) => {
+        if (error) {
+            console.error("Error counting documents:", error);
+            response.status(500).send("Internal Server Error");
+        } else {
+            const body = request.body;
+
+            database.collection("References").insertOne(body, (insertError) => {
+                if (insertError) {
+                    console.error("Error adding note:", insertError);
+                    response.status(500).send("Internal Server Error");
+                } else {
+                    console.log(body);
+                    response.json("Note added successfully");
+                }
+            });
+        }
+    });
+});
+
+/* 
+Function: Get a reference list based on a projectID, and return the array of ids.
+Return: References's in a MondoDB _id list
+*/
+app.get('/api/user/viewReferencesByProject', (request, response) => {
+    const referenceID = request.query.referenceID;
+    console.log(request.query.referenceID);
+    database.collection('References').find({"projectID": referenceID}).toArray((error, result) => {
+      if (error) {
+        console.error('Error retrieving data from MongoDB:', error);
+        response.status(500).send('Internal Server Error');
+      } else {
+        // Send the result as a response
+        response.send(result);
+      }
+    });
+  });
+
+
+  /* 
+Function: Get a reference list based on a taskID, and return the array of ids.
+Return: References's in a MondoDB _id list
+*/
+app.get('/api/user/viewReferencesByTask', (request, response) => {
+    const referenceID = request.query.referenceID;
+    console.log(request.query.referenceID);
+    database.collection('References').find({"taskID": referenceID}).toArray((error, result) => {
+      if (error) {
+        console.error('Error retrieving data from MongoDB:', error);
+        response.status(500).send('Internal Server Error');
+      } else {
+        // Send the result as a response
+        response.send(result);
+      }
+    });
+  });
+
+
+/* 
+Function: Delete a reference based on a given referenceID
+*/
+app.delete('/api/user/deleteReferences', (request, response) =>{
+    const referenceID = request.query.referenceID;
+    console.log(request.query.referenceID);
+    database.collection("References").deleteOne(referenceID, (error,result) =>
+    {
+        if (error) {
+            console.error('Error deleting data from MongoDB:',error);
+            response.status(500).send('Internal Server Error');
+        } else {
+            if (result.deletedCount === 0) {
+                response.status(400).send('Project not found');
+            } else {
+                response.status(200).send('Project deleted');
+            }
+        }
+    });
+});
